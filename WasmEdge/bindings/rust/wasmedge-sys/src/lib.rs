64cafa4db07b1c651070ce755e14abee3d4be7e7
Zhenghao Lu <54395432+EmisonLu@users.noreply.github.com>
2022-09-06 13:20:01 +0800

    Update `MAX_HOST_FUNC_LENGTH` to 2000

--- WasmEdge/bindings/rust/wasmedge-sys/src/lib.rs
+++ WasmEdge/bindings/rust/wasmedge-sys/src/lib.rs
@@ -234,7 +234,7 @@ lazy_static! {
                 .map(|s| s
                     .parse::<usize>()
                     .expect("MAX_HOST_FUNC_LENGTH should be a positive integer."))
-                .unwrap_or(500)
+                .unwrap_or(2000)
         )));
 }
 
